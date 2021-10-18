93a3d94e94d4db3156241ef458e3fbb185410857
Squiblydoo <squiblydoo@pm.me>
2021-10-18 08:50:16 -0400

    Added unzipping functionality
    Added the ability to unzip downloaded files automatically.

--- bazaarShopper.py
+++ bazaarShopper.py
@@ -7,11 +7,15 @@ import requests
 import json
 import argparse
 import os
+import pyzipper
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description = "Pull samples from MalwareBazzar by specifying tag associated with the malware and the number of samples to download. If no number is specified the default 50 samples will be used. ")
 parser.add_argument("tag", help="Specify the tag used on MalwareBazaar to identify the malware to download.")
 parser.add_argument("--limit", "-l", default="50", type=int, help="Specify the maximum limit for pulling samples. Default is 50")
 parser.add_argument("--sample_directory", "-s", default=".", help="Specify a directory for saving the samples. Defaults to current directory.")
+parser.add_argument("--unzip", "-u",  help="Specify a directory  for saving unzipped samples. Defaults to creating a directory named Unzipped.")
+parser.add_argument("--password", "-p", default="infected", help="Specify a password for unzipping files. Defaults to 'infected'")
 args = parser.parse_args()
 
 #Make request for Data
@@ -24,6 +28,9 @@ dataRequest.raise_for_status()
 #Convert response to form usable by python script
 jsonString = dataRequest.text
 jsonPythonValue = json.loads(jsonString)
+
+#Save the current directory incase we need it later
+cwd = os.getcwd()
 
 # If a directory was specified for saving samples, check if it exists. If it does not exist
 # create the directory and then move into the directory.
@@ -40,4 +47,12 @@ for i in range(len(jsonPythonValue["data"])):
     filetosave = open(jsonPythonValue["data"][i]["sha256_hash"], "wb")
     filetosave.write(fileRequest.content)
     filetosave.close()
+
+if (args.unzip != None):
+    if (os.getcwd != cwd):
+        os.chdir(cwd)
+    for zippedfile in os.listdir(args.sample_directory):
+        with pyzipper.AESZipFile(args.sample_directory + "\\"+ zippedfile, 'r') as zf:
+            zf.pwd = bytes(args.password, "UTF-8")
+            zf.extractall(args.unzip)
 
